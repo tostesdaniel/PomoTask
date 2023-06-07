cbc7ebcc67773c35eaf7f861218c1aaa5ec62f81
Daniel Tostes <ohtosttnz@gmail.com>
2023-06-07 10:49:10 -0300

    styles: change margin and padding to 0

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,8 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   * {
-    margin: 10px;
-    padding: 10px;
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
   }
 

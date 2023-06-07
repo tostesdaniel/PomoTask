e6c4318fa62f7f62c2f4b1f22e2ddcb8a74f048e
Daniel Tostes <ohtosttnz@gmail.com>
2023-06-07 11:53:11 -0300

    feat: use LayoutContainer on DefaultLayout

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../components/Header";
+import Header from "../../components";
+import { LayoutContainer } from "./styles";
 
 export default function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   );
 }

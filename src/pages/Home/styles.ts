cf94924a1b8713aa028c75dc7fc071aad5017c1a
Daniel Tostes <ohtosttnz@gmail.com>
2023-06-07 12:46:31 -0300

    feat: create styled components for home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,84 @@
+import { styled } from "styled-components";
+
+export const HomeContainer = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`;
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+
+  color: ${(props) => props.theme["gray-100"]};
+  font-size: 1.125rem;
+  font-weight: bold;
+`;
+
+export const CountdownContainer = styled.div`
+  font-family: "Roboto Mono", monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(props) => props.theme["gray-700"]};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+
+  span:nth-child(3) {
+    background: none;
+    padding: 2rem 0;
+    color: ${(props) => props.theme.accent};
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  border-radius: 8px;
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme.accent};
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: darken(${(props) => props.theme.accent}, 10%);
+  }
+`;

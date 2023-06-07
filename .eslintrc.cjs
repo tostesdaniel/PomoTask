2153ae7d958172946012dcb80804a91d22c43b40
Daniel Tostes <ohtosttnz@gmail.com>
2023-06-07 10:16:58 -0300

    chore(eslint): extend eslint-plugin-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,9 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",

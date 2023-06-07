0c9f6ae3f939faeaa8c19b854fe07f0922e30e6a
Daniel Tostes <ohtosttnz@gmail.com>
2023-06-07 12:46:56 -0300

    feat: update home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,38 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  StartCountdownButton,
+} from "./styles";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="text" id="minutesAmount" />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
 }

278b4dde423b569d70cb4b2d715922d7c78b9edf
Daniel Tostes <ohtosttnz@gmail.com>
2023-07-03 19:33:07 -0300

    refactor: extract cycles logic to own context

---
+++ src/contexts/CycleContext.tsx
@@ -0,0 +1,98 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface CreateCycleData {
+  task: string;
+  minutesAmount: number;
+}
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
+
+interface CycleContextType {
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  setActiveCycleId: React.Dispatch<React.SetStateAction<string | null>>;
+  setCycles: React.Dispatch<React.SetStateAction<Cycle[]>>;
+  secondsPassed: number;
+  setSecondsPassed: React.Dispatch<React.SetStateAction<number>>;
+  createNewCycle: (data: CreateCycleData) => void;
+  interruptCycle: () => void;
+}
+
+export const CyclesContext = createContext({} as CycleContextType);
+
+interface CyclesContextProviderProps {
+  children: ReactNode;
+}
+
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [secondsPassed, setSecondsPassed] = useState(0);
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    setCycles((prevCycles) => [...prevCycles, newCycle]);
+    setActiveCycleId(id);
+    setSecondsPassed(0);
+  }
+
+  function interruptCycle() {
+    if (activeCycle) {
+      const interruptedCycle = {
+        ...activeCycle,
+        interruptedDate: new Date(),
+      };
+
+      setCycles((prevCycles) => {
+        const updatedCycles = prevCycles.map((cycle) => {
+          if (cycle.id === activeCycleId) {
+            return interruptedCycle;
+          } else {
+            return cycle;
+          }
+        });
+
+        return updatedCycles;
+      });
+
+      setActiveCycleId(null);
+      setSecondsPassed(0);
+    }
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        activeCycle,
+        activeCycleId,
+        setActiveCycleId,
+        setCycles,
+        secondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
+}
